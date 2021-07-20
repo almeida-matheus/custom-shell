4e6f5e9c725cceb6591d602e02965b49447530e7
almeida-matheus <matheusalmeidacosta1212@gmail.com>
2021-07-20 19:41:29 -0300

    add reset function

--- custom-bash.sh
+++ custom-bash.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
 
-# chmod +x customize_bash.sh
-# ./customize_bash.sh
-
 PrepareRun() {
-    cat ~/.bash_profile | grep mac8028 > /dev/null 2>&1
-    if [ $? == 0 ]; then
-        echo ""
-        echo "bash customization has already been applied to user $USER"
-        echo -n "do you wanna reset your bash? [y/N]: "
-        read reset;
-        echo ""
-        if [ "$reset" == "y" ] || [ "$reset" == "Y" ]; then
-            rm ~/.bash_profile && rm ~/.bashrc
-            touch ~/.bash_profile && touch ~/.bashrc
-            return
-        fi
+cat ~/.bash_profile | grep mac8028 > /dev/null 2>&1
+if [ $? == 0 ]; then
+    echo ""
+    echo "bash customization has already been applied to user $USER"
+    echo -n "do you wanna reset your bash? [y/N]: "
+    read reset;
+    echo ""
+    # if [[ $reset == "y" ]] || [[ $reset == "Y" ]]; then
+    if ! { [ "$reset" == "y" ] || [ "$reset" == "Y" ]; }; then
         exit 1
     fi
+    cp ~/.bash_profile ~/.bash_profile.bkp && cp ~/.bashrc ~/.bashrc.bkp
+    rm ~/.bash_profile && rm ~/.bashrc
+    touch ~/.bash_profile && touch ~/.bashrc
+else
     if [ ! -f ~/.bashrc ]; then
         touch ~/.bashrc
     fi
@@ -25,33 +23,34 @@ PrepareRun() {
         touch ~/.bash_profile
     fi
     cp ~/.bash_profile ~/.bash_profile.bkp && cp ~/.bashrc ~/.bashrc.bkp
+fi
 }
 
 DefaultShell() {
-    shell="$SHELL"
-    if [ $shell != "/bin/bash" ]
-    then
-        echo -e "default shell of user $USER is zsh"
-        echo -n "do you wanna change your default shell to bash? [y/N]: "
-        read change;
-        echo ""
-        if [ "$change" == "y" ] || [ "$change" == "Y" ] || [ "$change" == "" ]; then
-            echo -e "so now you must enter the username and then enter your password"
-            chsh -s /bin/bash
-            echo -e "now your default shell is bash"
-            echo "if you wanna change to zsh please enter ' chsh -s /bin/zsh ' in the terminal"
-            return
-        fi
-        echo -e "your default shell is zsh"
-        echo "if you wanna change to bash please enter ' chsh -s /bin/bash ' in the terminal"
-    else
-        echo ""
-        echo -e "default shell of user $USER is bash"
+shell="$SHELL"
+if [ $shell != "/bin/bash" ]
+then
+    echo -e "default shell of user $USER is zsh"
+    echo -n "do you wanna change your default shell to bash? [y/N]: "
+    read change;
+    echo ""
+    if [ "$change" == "y" ] || [ "$change" == "Y" ] || [ "$change" == "" ]; then
+        echo -e "so now you must enter the username and then enter your password"
+        chsh -s /bin/bash
+        echo -e "now your default shell is bash"
+        echo "if you wanna change to zsh please enter ' chsh -s /bin/zsh ' in the terminal"
+        return
     fi
+    echo -e "your default shell is zsh"
+    echo "if you wanna change to bash please enter ' chsh -s /bin/bash ' in the terminal"
+else
+    echo ""
+    echo -e "default shell of user $USER is bash"
+fi
 }
 
 ApplyCustom() {
-    echo "
+echo "
 # mac8028 customize prompt
 show_branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/─[\1]/'
@@ -61,15 +60,18 @@ if [ $(id -u) == 0 ]; then
 else
     export PS1='\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[92m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]'
 fi
-    " >> ~/.bash_profile
-    echo "
+" >> ~/.bash_profile
+echo "
 # mac8028 customize prompt
 show_branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/─[\1]/'
 }
 export PS1='\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[93m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]'
-    " > ~/.bashrc
-    echo "
+" > ~/.bashrc
+echo "
+# enable lscolor
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
 # alias useful
 alias ls='ls -G'
 alias ll='ls -lhaCF'
@@ -88,7 +90,7 @@ export LESS_TERMCAP_us=$'\e[1;4;31m'
 export HISTTIMEFORMAT='%d/%m/%y %T '
 # don't put duplicate lines or lines starting with space in the history.
 HISTCONTROL=ignoreboth
-    " | tee -a ~/.bash_profile ~/.bashrc > /dev/null
+" | tee -a ~/.bash_profile ~/.bashrc > /dev/null
 }
 
 PrepareRun
