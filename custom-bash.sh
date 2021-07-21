3cc3b656efa948f1107c586ab47437281ac172ac
almeida-matheus <matheusalmeidacosta1212@gmail.com>
2021-07-21 00:16:13 -0300

    add script for linux - mac

--- custom-bash.sh
+++ custom-bash.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 PrepareRun() {
-cat ~/.bash_profile | grep mac8028 > /dev/null 2>&1
+cat $1 | grep mac8028 > /dev/null 2>&1
 if [ $? == 0 ]; then
     echo ""
     echo "bash customization has already been applied to user $USER"
     echo -n "do you wanna reset your bash? [y/N]: "
     read reset;
     echo ""
-    # if [[ $reset == "y" ]] || [[ $reset == "Y" ]]; then
     if ! { [ "$reset" == "y" ] || [ "$reset" == "Y" ]; }; then
         exit 1
     fi
@@ -21,14 +20,14 @@ else
     fi
     if [ ! -f ~/.bash_profile ]; then
         touch ~/.bash_profile
+        echo "teste"
     fi
     cp ~/.bash_profile ~/.bash_profile.bkp && cp ~/.bashrc ~/.bashrc.bkp
 fi
 }
 
 DefaultShell() {
-shell="$SHELL"
-if [ $shell != "/bin/bash" ]
+if [ $SHELL != "/bin/bash" ]
 then
     echo -e "default shell of user $USER is zsh"
     echo -n "do you wanna change your default shell to bash? [y/N]: "
@@ -49,7 +48,7 @@ else
 fi
 }
 
-ApplyCustom() {
+ApplyCustomMac() {
 echo "
 # mac8028 customize prompt
 show_branch() {
@@ -93,11 +92,67 @@ HISTCONTROL=ignoreboth
 " | tee -a ~/.bash_profile ~/.bashrc > /dev/null
 }
 
-PrepareRun
-DefaultShell
-ApplyCustom
+ApplyCustomLinux() {
+echo "
+# mac8028 customize prompt
+show_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/─[\1]/'
+}
+if [ $(id -u) == 0 ]; then
+    export PS1='\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[91m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]'
+else
+    export PS1='\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[92m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]'
+fi
+" >> ~/.bash_profile
+echo "
+# mac8028 customize prompt
+show_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/─[\1]/'
+}
+if [ $(id -u) == 0 ]; then
+    export PS1='\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[91m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]'
+else
+    export PS1='\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[92m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]'
+fi
+" > ~/.bashrc
+echo "
+# enable lscolor
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
+# alias useful
+alias ls='ls -G'
+alias ll='ls -lhaCF'
+alias grep='grep --color=always'
+# ignore case
+set completion-ignore-case On
+# customize man
+export LESS_TERMCAP_mb=$'\e[1;32m'
+export LESS_TERMCAP_md=$'\e[1;32m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[01;33m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[1;4;31m'
+# customize history
+export HISTTIMEFORMAT='%d/%m/%y %T '
+# don't put duplicate lines or lines starting with space in the history.
+HISTCONTROL=ignoreboth
+" | tee -a ~/.bash_profile ~/.bashrc > /dev/null
+}
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    bash_file=~/.bashrc
+    PrepareRun $bash_file
+    DefaultShell
+    ApplyCustomLinux
+else
+    bash_file=~/.bash_profile
+    PrepareRun $bash_file
+    DefaultShell
+    ApplyCustomMac
+fi
+
 source ~/.bash_profile
 source ~/.bashrc
 
 echo "modifications successfully performed, restart the terminal to apply the modifications"
-echo ""
