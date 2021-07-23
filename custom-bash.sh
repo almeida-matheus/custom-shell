77ea09d29dae61fe0a152de5cc795276e7b725ea
almeida-matheus <matheusalmeidacosta1212@gmail.com>
2021-07-22 23:59:06 -0300

    add custom bash for linux

--- custom-bash.sh
+++ custom-bash.sh
@@ -20,7 +20,6 @@ else
     fi
     if [ ! -f ~/.bash_profile ]; then
         touch ~/.bash_profile
-        echo "teste"
     fi
     cp ~/.bash_profile ~/.bash_profile.bkp && cp ~/.bashrc ~/.bashrc.bkp
 fi
@@ -32,7 +31,7 @@ then
     echo -e "default shell of user $USER is zsh"
     echo -n "do you wanna change your default shell to bash? [y/N]: "
     read change;
-    echo ""
+    # echo ""
     if [ "$change" == "y" ] || [ "$change" == "Y" ] || [ "$change" == "" ]; then
         echo -e "so now you must enter the username and then enter your password"
         chsh -s /bin/bash
@@ -49,98 +48,106 @@ fi
 }
 
 ApplyCustomMac() {
-echo "
+echo '
 # mac8028 customize prompt
 show_branch() {
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/─[\1]/'
+    git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/─[\1]/"
 }
 if [ $(id -u) == 0 ]; then
-    export PS1='\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[91m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]'
+    export PS1="\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[91m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
 else
-    export PS1='\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[92m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]'
+    export PS1="\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[92m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
 fi
-" >> ~/.bash_profile
-echo "
+' >> ~/.bash_profile
+echo '
 # mac8028 customize prompt
 show_branch() {
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/─[\1]/'
+    git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/─[\1]/"
 }
-export PS1='\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[93m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]'
-" > ~/.bashrc
-echo "
+export PS1="\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[93m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]" 
+' >> ~/.bashrc
+echo '
+# ignore case
+set completion-ignore-case On
+# alias useful
+alias ls="ls -G"
+alias ll="ls -lhaCF" 
+alias grep="grep --color=always"
 # enable lscolor
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
-# alias useful
-alias ls='ls -G'
-alias ll='ls -lhaCF'
-alias grep='grep --color=always'
-# ignore case
-set completion-ignore-case On
 # customize man
-export LESS_TERMCAP_mb=$'\e[1;32m'
-export LESS_TERMCAP_md=$'\e[1;32m'
-export LESS_TERMCAP_me=$'\e[0m'
-export LESS_TERMCAP_se=$'\e[0m'
-export LESS_TERMCAP_so=$'\e[01;33m'
-export LESS_TERMCAP_ue=$'\e[0m'
-export LESS_TERMCAP_us=$'\e[1;4;31m'
+export LESS_TERMCAP_mb=$"\e[1;32m" 
+export LESS_TERMCAP_md=$"\e[1;32m" 
+export LESS_TERMCAP_me=$"\e[0m" 
+export LESS_TERMCAP_se=$"\e[0m"  
+export LESS_TERMCAP_so=$"\e[01;33m" 
+export LESS_TERMCAP_ue=$"\e[0m" 
+export LESS_TERMCAP_us=$"\e[1;4;31m"
 # customize history
-export HISTTIMEFORMAT='%d/%m/%y %T '
-# don't put duplicate lines or lines starting with space in the history.
+export HISTTIMEFORMAT="%d/%m/%y %T " 
+# dont put duplicate lines or lines starting with space in the history.
 HISTCONTROL=ignoreboth
-" | tee -a ~/.bash_profile ~/.bashrc > /dev/null
+' | tee -a ~/.bash_profile ~/.bashrc > /dev/null
 }
 
 ApplyCustomLinux() {
-echo "
+echo '
 # mac8028 customize prompt
 show_branch() {
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/─[\1]/'
+    git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/─[\1]/"
 }
 if [ $(id -u) == 0 ]; then
-    export PS1='\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[91m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]'
+    export PS1="\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[91m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
 else
-    export PS1='\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[92m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]'
+    export PS1="\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[92m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
 fi
-" >> ~/.bash_profile
-echo "
+' >> ~/.bash_profile
+echo ' 
 # mac8028 customize prompt
-show_branch() {
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/─[\1]/'
+show_branch() { 
+    git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/─[\1]/"
 }
 if [ $(id -u) == 0 ]; then
-    export PS1='\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[91m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]'
+    export PS1="┌──\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\[$(tput sgr0)\$(__git_ps1 "─[%s]") \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[91m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
 else
-    export PS1='\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[92m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]'
+    export PS1="┌──\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\[$(tput sgr0)\$(__git_ps1 "─[%s]") \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[92m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
 fi
-" > ~/.bashrc
-echo "
+' >> ~/.bashrc
+echo '
+# ignore case
+set completion-ignore-case On
+# alias useful
+alias ls="ls --color=auto" 
+alias grep="grep --color=auto" 
+alias diff="diff --color=auto"
 # enable lscolor
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
-# alias useful
-alias ls='ls -G'
-alias ll='ls -lhaCF'
-alias grep='grep --color=always'
-# ignore case
-set completion-ignore-case On
 # customize man
-export LESS_TERMCAP_mb=$'\e[1;32m'
-export LESS_TERMCAP_md=$'\e[1;32m'
-export LESS_TERMCAP_me=$'\e[0m'
-export LESS_TERMCAP_se=$'\e[0m'
-export LESS_TERMCAP_so=$'\e[01;33m'
-export LESS_TERMCAP_ue=$'\e[0m'
-export LESS_TERMCAP_us=$'\e[1;4;31m'
+export LESS_TERMCAP_mb=$"\e[1;32m" 
+export LESS_TERMCAP_md=$"\e[1;32m" 
+export LESS_TERMCAP_me=$"\e[0m" 
+export LESS_TERMCAP_se=$"\e[0m"  
+export LESS_TERMCAP_so=$"\e[01;33m" 
+export LESS_TERMCAP_ue=$"\e[0m" 
+export LESS_TERMCAP_us=$"\e[1;4;31m"
 # customize history
-export HISTTIMEFORMAT='%d/%m/%y %T '
-# don't put duplicate lines or lines starting with space in the history.
+export HISTTIMEFORMAT="%d/%m/%y %T " 
+# dont put duplicate lines or lines starting with space in the history.
 HISTCONTROL=ignoreboth
-" | tee -a ~/.bash_profile ~/.bashrc > /dev/null
+# enable programmable completion features
+if ! shopt -oq posix; then
+    if [ -f /usr/share/bash-completion/bash_completion ]; then
+        . /usr/share/bash-completion/bash_completion
+    elif [ -f /etc/bash_completion ]; then
+        . /etc/bash_completion
+    fi
+fi
+' | tee -a ~/.bash_profile ~/.bashrc > /dev/null
 }
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
+if [[ $OSTYPE == "linux-gnu" ]]; then
     bash_file=~/.bashrc
     PrepareRun $bash_file
     DefaultShell
