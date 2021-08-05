a41603b2d5ce2e1c7d1d58030d235ee155410459
almeida-matheus <matheusalmeidacosta1212@gmail.com>
2021-08-04 23:54:30 -0300

    fix prompt

--- custom-bash.sh
+++ custom-bash.sh
@@ -7,18 +7,15 @@ if [ $? == 0 ]; then
     echo -n "do you wanna reset your bash? [y/N]: "
     read reset;
     if ! { [ "$reset" == "y" ] || [ "$reset" == "Y" ]; }; then
+        echo ""
         exit 1
     fi
     cp ~/.bash_profile ~/.bash_profile.bkp && cp ~/.bashrc ~/.bashrc.bkp
     rm ~/.bash_profile && rm ~/.bashrc
     touch ~/.bash_profile && touch ~/.bashrc
 else
-    if [ ! -f ~/.bashrc ]; then
-        touch ~/.bashrc
-    fi
-    if [ ! -f ~/.bash_profile ]; then
-        touch ~/.bash_profile
-    fi
+    if [ ! -f ~/.bashrc ]; then touch ~/.bashrc; fi
+    if [ ! -f ~/.bash_profile ]; then touch ~/.bash_profile; fi
     if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
     cp ~/.bash_profile ~/.bash_profile.bkp && cp ~/.bashrc ~/.bashrc.bkp
 fi
@@ -26,17 +23,13 @@ fi
 
 DefaultShell() {
 if [ $SHELL != "/bin/bash" ]; then
-    echo -e "\ndefault shell of user $USER is zsh"
+    echo -e "\ndefault shell of user $USER is not bash"
     echo -n "do you wanna change your default shell to bash? [y/N]: "
     read change;
     if [ "$change" == "y" ] || [ "$change" == "Y" ] || [ "$change" == "" ]; then
         chsh -s /bin/bash
-        echo -e "\ndefault shell of user $USER is bash"
         return
     fi
-    echo -e "\ndefault shell of user $USER is zsh"
-else
-    echo -e "\ndefault shell of user $USER is bash"
 fi
 }
 
@@ -44,20 +37,22 @@ ApplyCustomMac() {
 echo '
 # custom-bash :)
 show_branch() {
-    git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/─[\1]/"
+    local gbranch=$(git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/[\1]/")
+    if [ "$gbranch" != "" ]; then echo "─$(tput bold)$gbranch"; fi
 }
 if [ $(id -u) == 0 ]; then
-    export PS1="\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[91m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
+    export PS1="\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\[$(tput sgr0)\]\$(show_branch)\[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[91m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
 else
-    export PS1="\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[92m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
+    export PS1="\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\[$(tput sgr0)\]\$(show_branch)\[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[92m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
 fi
 ' >> ~/.bash_profile
 echo '
 # custom-bash :)
 show_branch() {
-    git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/─[\1]/"
+    local gbranch=$(git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/[\1]/")
+    if [ "$gbranch" != "" ]; then echo "─$(tput bold)$gbranch"; fi
 }
-export PS1="\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[93m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]" 
+export PS1="\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\[$(tput sgr0)\]\$(show_branch)\[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[92m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
 ' >> ~/.bashrc
 echo '
 set completion-ignore-case On
@@ -70,16 +65,18 @@ export HISTTIMEFORMAT="%d/%m/%y %T "
 ' | tee -a ~/.bash_profile ~/.bashrc > /dev/null
 echo 'set completion-ignore-case On' >> ~/.inputrc
 }
+
 ApplyCustomLinux() {
-echo ' 
+echo '
 # custom-bash :)
 show_branch() {
-    git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/ [\1]/"
+    local gbranch=$(git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/[\1]/")
+    if [ "$gbranch" != "" ]; then echo "─$(tput bold)$gbranch"; fi
 }
 if [ $(id -u) == 0 ]; then
-    export PS1="\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[91m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
+    export PS1="\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\[$(tput sgr0)\]\$(show_branch)\[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[91m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
 else
-    export PS1="\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\$(show_branch) \[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[92m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
+    export PS1="\[$(tput sgr0)\]┌─\[$(tput bold)\](\u@\h)\[$(tput sgr0)\]─\[$(tput bold)\][\w]\[$(tput sgr0)\]\$(show_branch)\[$(tput sgr0)\]\n└─\[$(tput sgr0)\]\[$(tput bold)\]\[\033[92m\]\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
 fi
 set completion-ignore-case On
 alias ls="ls --color=auto"
@@ -108,4 +105,4 @@ fi
 source ~/.bash_profile
 source ~/.bashrc
 
-echo -e "please restart the terminal to apply the modifications\n"
+echo -e "\nrestart the terminal to apply the modifications\n"
